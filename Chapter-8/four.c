b4de1633404c5ae6f35e9652c9ded42cfb05c67d
Wallys Ferreira <wallysfsilva@gmail.com>
2023-07-18 18:34:40 -0300

    chapter 8 projects

--- Chapter-8/four.c
+++ Chapter-8/four.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 
+#define LENGTH ((int) (sizeof(a) / sizeof(a[0])))
+#define N 10
+
 int main(void) {
+	int a[N];
+
+	printf("Enter %d numbers: ", N);
+	for (int i = 0; i < N; i++) {
+		scanf("%d", &a[i]);
+	}
+
+	printf("In reverse order: ");
+	for (int i = LENGTH - 1; i >= 0; i--) {
+		printf("%d ", a[i]);
+	}
+
+	printf("\n");
 
 	return 0;
 }

b062eccbfb4a85bb1711018819a7d4158d0bc125
Wallys Ferreira <wallysfsilva@gmail.com>
2023-07-21 18:01:51 -0300

    chapter 8 projects

---
+++ Chapter-8/fourteen.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+#define WORDS 12
+#define LETTERS 10
+
+int main(void) {
+	char sentence[WORDS][LETTERS] = {'n'}, final;
+
+	printf("Enter a sentence: ");
+	int finished = 0;
+	for (int i = 0; i < WORDS; i++) {
+		for (int j = 0; j < LETTERS; j++) {
+			scanf("%c", &sentence[i][j]);
+
+			if (sentence[i][j] == '\n' || sentence[i][j] == '.' || sentence[i][j] == '?' || sentence[i][j] == '!') {
+				final = sentence[i][j];
+				sentence[i][j] = ' ';
+				finished = 1;
+				break;
+			}
+
+			if (sentence[i][j] == ' ') {
+				break;
+			}
+		}
+
+		if (finished) {
+			break;
+		}
+	}
+
+	printf("Reversal of sentence: ");
+	for (int i = WORDS - 1; i >= 0; i--) {
+		for (int j = 0; j < LETTERS; j++) {
+			//printf("%d %d\n", i, j);
+			if (i == 0 && sentence[i][j] == ' ') {
+				sentence[i][j] = final;
+			}
+
+
+			if (sentence[i][j] != '\n') {
+				printf("%c", sentence[i][j]);
+			}
+		}
+	}
+
+	printf("\n");
+
+	return 0;
+}
--- Chapter-8/thirteen.c
+++ Chapter-8/thirteen.c
@@ -1,6 +1,36 @@
 #include <stdio.h>
 
+#define LENGTH ((int) (sizeof(last_name) / sizeof(last_name[0])))
+
 int main(void) {
+	char last_name[20], first_letter, placeholder;
+	int space_happened;
+
+	printf("Enter a first and last name: ");
+	first_letter = getchar();
+	
+	do {
+		scanf("%c", &placeholder);
+	} while (placeholder != ' ');
+
+	for (int i = 0; i < 20; i++) {
+		scanf("%c", &last_name[i]);
+
+		if (last_name[i] == '\n') {
+			last_name[i] = ',';
+			last_name[i + 1] = ' ';
+			break;
+		}
+	}
+
+	printf("You entered the name: ");
+	for (int i = 0; i < LENGTH; i++) {
+		if (last_name[i] == ' ') {
+			break;
+		}
+		printf("%c", last_name[i]);
+	}
+	printf(" %c\n", first_letter);
 
 	return 0;
 }

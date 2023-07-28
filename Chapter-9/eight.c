ae3eba43abc1c4c02925dffea2cb218b1034fb9f
Wallys Ferreira <wallysfsilva@gmail.com>
2023-07-28 01:33:43 -0300

    chapter 9 projects

--- Chapter-9/eight.c
+++ Chapter-9/eight.c
@@ -9,6 +9,23 @@ int roll_dice(void);
 bool play_game(void);
 
 int main(void) {
+	bool keep_going = TRUE;
+	int wins = 0, losses = 0;
+
+	while (keep_going) {
+		int result = play_game();
+
+		// Check for the result of the game
+		if (result) {
+			printf("You win!\n");
+			wins++;
+		} else {
+			printf("You lose!\n");
+			losses++;
+		}
+	}
+
+	printf("Wins: %d	Losses: %d\n", wins, losses);
 
 	return 0;
 }

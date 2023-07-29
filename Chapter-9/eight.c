8134bee7f3d5e6584adf6d82037de27bf3c6d796
Wallys Ferreira <wallysfsilva@gmail.com>
2023-07-29 19:24:44 -0300

    chapter 9 projects

--- Chapter-9/eight.c
+++ Chapter-9/eight.c
@@ -1,4 +1,7 @@
+#include <ctype.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 #define TRUE 1;
 #define FALSE 0;
@@ -9,10 +12,10 @@ int roll_dice(void);
 bool play_game(void);
 
 int main(void) {
-	bool keep_going = TRUE;
+	char keep_going = 'Y';
 	int wins = 0, losses = 0;
 
-	while (keep_going) {
+	while (keep_going == 'Y') {
 		int result = play_game();
 
 		// Check for the result of the game
@@ -23,9 +26,44 @@ int main(void) {
 			printf("You lose!\n");
 			losses++;
 		}
+	
+		// Check if user wants to keep playing
+		printf("\nPlay again? ");
+		scanf(" %c", &keep_going);
+		keep_going = toupper(keep_going);
+		printf("\n");
 	}
 
 	printf("Wins: %d	Losses: %d\n", wins, losses);
 
 	return 0;
+}
+
+int roll_dice(void) {
+	srand(clock());
+
+	int roll1 = rand() % 6 + 1;
+	int roll2 = rand() % 6 + 1;
+
+	return roll1 + roll2;
+}
+
+bool play_game(void) {
+	int roll = roll_dice(), round = 1, last = 0;
+
+	printf("You rolled: %d\n", roll);
+
+	while (1) {
+		if (round == 1 && (roll == 7 || roll == 11)) {
+			return TRUE;
+		}
+
+		last = roll;
+		roll = roll_dice();
+		printf("You rolled: %d\n", roll);
+		round++;
+
+		if (roll == 7) return FALSE;
+		if (roll == last) return TRUE;
+	}
 }

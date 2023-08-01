026677599c7d04732081395bb2b9cd712965a2c4
Wallys Ferreira <wallysfsilva@gmail.com>
2023-07-31 21:27:09 -0300

    chapter 10 projects

--- Chapter-10/two.c
+++ Chapter-10/two.c
@@ -7,27 +7,28 @@
 #define NUM_CARDS 5
 
 // External variables
-int num_in_rank[NUM_RANKS];
-int num_in_suit[NUM_SUITS];
 bool straight, flush, four ,three;
 int pairs; // Can be 0, 1 or 2
 
 // Prototypes
-void read_cards(void);
-void analyze_hand(void);
+void read_cards(int num_in_rank[NUM_RANKS], int num_in_suit[NUM_SUITS]);
+void analyze_hand(int num_in_rank[NUM_RANKS], int num_in_suit[NUM_SUITS]);
 void print_result(void);
 
 // main: Calls read_cards, analyze_hand, and print_result repeatedly.
 int main(void) {
+	int num_in_rank[NUM_RANKS];
+	int num_in_suit[NUM_SUITS];
+
 	for (;;) {
-		read_cards();
-		analyze_hand();
+		read_cards(num_in_rank, num_in_suit);
+		analyze_hand(num_in_rank, num_in_suit);
 		print_result();
 	}
 }
 
 // read_cards: Reads the cards into the external variables num_in_rank and num_in_suit; checks for bad cards and duplicate cards
-void read_cards(void) {
+void read_cards(int num_in_rank[NUM_RANKS], int num_in_suit[NUM_SUITS]) {
 	bool cards_exists[NUM_RANKS][NUM_SUITS];
 	char ch, rank_ch, suit_ch;
 	int rank, suit;
@@ -92,7 +93,7 @@ void read_cards(void) {
 }
 
 // analyze_hand: Determines whether the hand contains a straight, a flush, four-of-a-kind, and/or three-of-a-kind; determines the number of paris; stores the results into the external variables straight, flush, four, three, and pairs.
-void analyze_hand(void) {
+void analyze_hand(int num_in_rank[NUM_RANKS], int num_in_suit[NUM_SUITS]) {
 	int num_consec = 0;
 	int rank, suit;
 

b376a1c5f9ecb6bfd85f70919a182efed1700976
Wallys Ferreira <wallysfsilva@gmail.com>
2023-08-02 18:17:27 -0300

    chapter 10 projects

--- Chapter-10/six.c
+++ Chapter-10/six.c
@@ -1,4 +1,17 @@
 #include <stdio.h>
+#include <stdbool.h>
+
+#define SIZE 100
+
+char contents[SIZE];
+int top = 0;
+
+void stack_overflow(void);
+void stack_underflow(void);
+bool is_empty(void);
+bool is_full(void);
+void push(char c);
+char pop(void);
 
 int main(void) {
 	printf("Enter an RPN expression: ");

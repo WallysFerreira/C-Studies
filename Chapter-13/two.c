b29d820b107a44c42c1406b48a3f64aecb311578
Wallys Ferreira <wallysfsilva@gmail.com>
2023-08-14 15:27:05 -0300

    add month

--- Chapter-13/two.c
+++ Chapter-13/two.c
@@ -8,8 +8,8 @@ int read_line(char str[], int n);
 
 int main(void) {
 	char reminders[MAX_REMIND][MSG_LEN+3];
-	char day_str[4], hour_str[7], msg_str[MSG_LEN+1];
-	int day, hours, minutes, i, num_remind = 0;
+	char month_str[4], day_str[4], hour_str[7], msg_str[MSG_LEN+1];
+	int month, day, hours, minutes, i, num_remind = 0;
 
 	for (;;) {
 		if (num_remind == MAX_REMIND) {
@@ -17,15 +17,21 @@ int main(void) {
 			break;
 		}
 
-		printf("Enter day, 24-hour time and reminder: ");
-		scanf("%2d", &day);
-		if (day == 0) break;
+		printf("Enter month/day, 24-hour time and reminder: ");
+		scanf("%2d/%2d", &month, &day);
+		if (day == 0 || month == 0) break;
 		if (day < 0 || day > 31) {
 			printf("\nInvalid day\n");
 			while (getchar() != '\n');
 			continue;
 		}
 		sprintf(day_str, "%2d ", day);
+		if (month < 0 || month > 12) {
+			printf("\nInvalid month\n");
+			while (getchar() != '\n');
+			continue;
+		}
+		sprintf(month_str, "%.2d/", month);
 		scanf("%2d:%2d", &hours, &minutes);
 		if ((hours < 0 || hours > 23) || (minutes < 0 || minutes > 59)) {
 			printf("\nInvalid time\n");
@@ -42,14 +48,15 @@ int main(void) {
 			strcpy(reminders[j], reminders[j - 1]);
 		}
 
-		strcpy(reminders[i], day_str);
+		strcpy(reminders[i], month_str);
+		strcat(reminders[i], day_str);
 		strcat(reminders[i], hour_str);
 		strcat(reminders[i], msg_str);
 
 		num_remind++;
 	}
 
-	printf("\nDay Hour Reminder\n");
+	printf("\nDate  Hour   Reminder\n");
 	for (i = 0; i < num_remind; i++) {
 		printf("%s\n", reminders[i]);
 	}
